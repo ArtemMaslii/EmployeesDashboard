5e744764fbcf4510841f0e831147b8c3207bfdd0
Artem <maslijartei@gmail.com>
2023-11-02 13:19:27 +0100

    Add functionality to remove employee from the list

--- my-app/src/components/employees-list-item/employees-list-item.js
+++ my-app/src/components/employees-list-item/employees-list-item.js
@@ -25,7 +25,7 @@ class EmployeeListItem extends Component {
     }
 
    render() {
-        const {name, salary} = this.props;
+        const {name, salary, onDelete} = this.props;
         const {increase, rise} = this.state;
         const classNames = "list-group-item d-flex justify-content-between" + (increase ? " increase": "") + (rise ? " like": "");
 
@@ -41,7 +41,8 @@ class EmployeeListItem extends Component {
                     </button>
 
                     <button type="button"
-                        className="btn-trash btn-sm">
+                        className="btn-trash btn-sm"
+                        onClick={onDelete}>
                         <i className="fas fa-trash"></i>
                     </button>
                     <i className="fas fa-star"></i>
